8f6a97d41711c21af6610da7c885a676be549312
Riyad Arafat <riyad.elberkawy@gmail.com>
2024-04-08 01:03:32 +0200

    Add CallManagerPackage to ReactNativeHost.getPackages()

--- android/app/src/main/java/com/riyadelberkawy/callcycle/MainApplication.kt
+++ android/app/src/main/java/com/riyadelberkawy/callcycle/MainApplication.kt
@@ -24,10 +24,11 @@ class MainApplication : Application(), ReactApplication {
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
     this,
     object : DefaultReactNativeHost(this) {
-      override fun getPackages(): List<ReactPackage> {
-        val packages = PackageList(this).packages
-        packages.add(CallManagerPackage())
-        return packages
+      override fun getPackages(): List<ReactPackage> =
+      PackageList(this).packages.apply {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          add(CallManagerPackage())
       }
 
       override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"

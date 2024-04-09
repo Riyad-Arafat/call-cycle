3cf17b89d44f78bbee01f50bbd67a7c5f0aa475d
Riyad Arafat <riyad.elberkawy@gmail.com>
2024-04-09 03:37:09 +0200

    Update Gradle command in eas.json and handle audio focus in CallManagerConnection.java

--- modules/react-native-call-manager/android/src/main/java/com/callmanager/CallManagerConnection.java
+++ modules/react-native-call-manager/android/src/main/java/com/callmanager/CallManagerConnection.java
@@ -2,49 +2,31 @@ package com.callmanager;
 
 import android.telecom.Connection;
 import android.telecom.DisconnectCause;
+import android.media.AudioManager;
 
 public class CallManagerConnection extends Connection {
-    @Override
-    public void onShowIncomingCallUi() {
-        // Don't call super.onShowIncomingCallUi() to prevent the native UI from showing
-        // Handle showing incoming call UI here
-        // For example, you can show a custom UI using your own logic
-    }
+    private AudioManager audioManager;
 
     @Override
     public void onAnswer() {
         super.onAnswer();
-        // Handle the answer action
         setActive(); // Set the call to active state
-    }
 
-    @Override
-    public void onReject() {
-        super.onReject();
-        // Handle the reject action
-        setDisconnected(new DisconnectCause(DisconnectCause.REJECTED));
-        destroy();
-    }
-
-    @Override
-    public void onHold() {
-        super.onHold();
-        // Handle the hold action
-        setOnHold();
-    }
-
-    @Override
-    public void onUnhold() {
-        super.onUnhold();
-        setActive();
+        audioManager = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setMode(AudioManager.MODE_IN_CALL);
+        audioManager.requestAudioFocus(null, AudioManager.STREAM_VOICE_CALL, AudioManager.AUDIOFOCUS_GAIN);
     }
 
     @Override
     public void onDisconnect() {
         super.onDisconnect();
-        // Handle the disconnect action
         setDisconnected(new DisconnectCause(DisconnectCause.LOCAL));
-        destroy();
+
+        if (audioManager != null) {
+            audioManager.abandonAudioFocus(null);
+            audioManager.setMode(AudioManager.MODE_NORMAL);
+        }
+        destroy(); // Clean up the call
     }
 
 }
